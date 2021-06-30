84f914226af80cc20cd83998e6346a58b8b9beb3
orsolon <56239735+orsolon@users.noreply.github.com>
2021-06-29 21:49:35 -0300

    lista3_ok

--- tarefa1/index.js
+++ tarefa1/index.js
@@ -119,13 +119,13 @@ class Quadrado {
         let tamanhoAltura = altura;
 
         this.calcularArea = function () {
-            return console.log(tamanhoBase * tamanhoAltura);
+            return tamanhoBase * tamanhoAltura;
         }
     }
 }
 
 const quadrado = new Quadrado (20,20);
-quadrado.calcularArea();
+console.log(quadrado.calcularArea());
 
 // Crie uma classe chamada Cubo que herda da classe Quadrado da questão anterior, 
 // e tem como atributo adicional o comprimento. Crie, além do construtor, 
@@ -161,13 +161,30 @@ class Ponto2D {
         let pontox = x;
         let pontoy = y;
 
-        this.calcularDistancia = function (parametro) {
+        this.getX = () => pontox;
+        this.getY = () => pontoy;
 
-        }
+        // this.calcularDistancia = function (a,b) {
+        //     const dx = a.pontox - b.pontox;
+        //     const dy = a.pontoy - b.pontoy;
+        //     return Math.hypot(dx, dy);
+    }
+
+    calcularDistancia(ponto) {
+        const delta_x = Math.pow(this.getX() - ponto.getX(),2);
+        const delta_y = Math.pow(this.getY() - ponto.getY(),2);
+        return Math.sqrt(delta_x + delta_y);
     }
 }
 
-const parametro = new Ponto2D(20, 35);
+
+// const p1 = new Ponto2D(5, 5);
+// const p2 = new Ponto2D(10, 10);
+// p1.calcularDistancia();
+// p2.calcularDistancia();
+// console.log(Ponto2D.calcularDistancia(p1, p2));
+
+
 
 // Crie uma classe que herda de Ponto2D chamada Ponto3D que tem como atributo adicional "z", 
 // reimplemente o método calcularDistancia() na classe Ponto3D que desta vez 
@@ -176,16 +193,29 @@ const parametro = new Ponto2D(20, 35);
 //     e o mesmo valendo para y e z) é dada pela raiz quadrada de 
 //     ((xp1 - xp2)² + (yp1 -yp2)² + (zp1 - zp2)²).
 
-class Ponto3D {
+class Ponto3D extends Ponto2D {
     constructor(x, y, z) {
         super(x, y);
-        let pontoZ = z;
+        let pontoz = z;
 
-        this.calcularDistancia = function (parametro2) {
+        this.getZ = () => pontoz;
 
+        // this.calcularDistancia = function (a,b) {
+        //     const dx = a.pontox - b.pontox;
+        //     const dy = a.pontoy - b.pontoy;
+        //     const dz = a.pontoz - b.pontoz;
+        //     return Math.hypot(dx, dy, dz);
         }
-    }
+
+        calcularDistancia(ponto) {
+            const delta_x = Math.pow(this.getX() - ponto.getX(),2);
+            const delta_y = Math.pow(this.getY() - ponto.getY(),2);
+            const delta_z = Math.pow(this.getZ() - ponto.getZ(),2);
+            return Math.sqrt(delta_x + delta_y + delta_z);
+        }
+
 }
+
 
 //Simulação Browser API | JS
 
@@ -207,13 +237,29 @@ class Ponto3D {
 //e o método removeClass(className: string) para remover uma classe do array.
 
 class HTMLBaseElement {
-    constructor(tagName, id, classList, name, style){
-        if(isNaN(base)) throw "tagName precisa ser string";
-        let atr_tagName = tagName;
-        let atr_id = undefined;
+    constructor(tagName){
+        if(typeof _tagName != 'string') throw "tagName precisa ser string";
+        let _tagName = tagName;
+        if(typeof id != 'string') throw "id precisa ser string";
+        let id = undefined;
+        if(typeof classList != 'string') throw "classList precisa ser string";
+        let classList = [];
+        if(typeof name != 'string') throw "name precisa ser string";
+        let name = undefined;
+        if(typeof style != 'object' ) throw "style precisa ser um objeto";
+        let style = {};
 
+        this.addClass = function(className) {
+            classList.push() = className;
+        }
+        this.removeClass = function(className) {
+            classList.pop() = className;
+        }
     }
 }
+
+const teste = new HTMLBaseElement('div');
+
 
 
 
@@ -269,6 +315,89 @@ class HTMLButtonElement extends HTMLParentElement {
 
 class HTMLTableElement extends HTMLParentElement {
     constructor(){
-        
+
     }
 }
+
+
+
+//___________________Exercícios Fila_______________________
+// Faça um programa para gerenciar uma to-do list, 
+// crie uma classe para representar a lista de tarefas que faça uso da estrutura de fila 
+// para incluir tarefas e remover tarefas considerando que as primeiras tarefas adicionadas, 
+// são as primeiras a serem executadas e tão logo, as primeiras removidas da lista. 
+// O programa deve funcionar da seguinte forma:
+
+class TodoList {
+    constructor() {
+        let list = [];
+        // this.add = function (valor) { list.push(valor)}
+        this.add = value => list = [...list, value];
+
+        this.remove = () => {
+            const [head, ...tail] = list;
+            list = tail;
+            return head;
+        }
+        this.isEmpty = () => list.length === 0;
+    }
+}
+
+
+todoList.add('Lavar o carro');
+todoList.add('Levar o cachorro para passear');
+todoList.remove() 
+// Out: 'Lavar o carro'
+todoList.add('Fazer o almoço');
+todoList.add('Lavar a louça');
+todoList.remove() 
+// Out: 'Levar o cachorro para passear'
+todoList.remove() 
+// Out: 'Fazer o almoço'
+todoList.remove() 
+// Out: 'Lavar a louça'
+todoList.remove() 
+// Out: 'Não existem tarefas a fazer!!'
+
+
+
+// Crie uma classe chamada Queue (Fila) que possui os métodos push() para inserir elementos e 
+// pop() para retirar elementos. Além deste, crie um método chamado pushQueue() 
+// que recebe um objeto da classe Queue como parâmetro e usando apenas os métodos push e pop, 
+// transfira os elementos da fila recebida como parâmetro para a atual (this). 
+// Crie também um método toString() para exibir os elementos da fila. Ex:
+
+
+
+
+// Crie uma classe chamada Cozinha, com uma lista pedidos que deve ser incializada no construtor 
+// da classe como um array vazio, comportando-se como uma fila. Crie um método confirmarPedido() 
+// que recebe um pedido como parâmetro e adiciona à pedidos. Crie um método terminarPedido() 
+// que retira um pedido do array pedidos. A lista de pedidos da cozinha não deve ultrapassar 
+// 10 pedidos, caso haja tentativa de adicionar o 11º, use o throw para lançar um erro.
+
+// Crie uma classe Atendente, com o método anotarPedido() que deve receber um pedido (string) 
+// como parâmetro e armazena num array pedidosAConfirmar que deve ser atributo da classe e se 
+// comportar como uma fila. Crie também um método chamado enviarPedido que recebe uma instância 
+// da classe Cozinha, e deve chamar o método confirmarPedido da cozinha passando o primeiro 
+// pedido de pedidosAConfirmar como parâmetro. Caso a lista de pedidos ainda não tenha atingido o
+//  limite, o método deve também remover o pedido do array pedidosAConfirmar, caso contrário,
+//   o item fica no array.
+
+
+
+
+//Crie uma classe chamada Estacionamento que deve possui três atributos:
+
+// primeiroAndar: string[]
+// segundoAndar: string[]
+// terceiroAndar: string[]
+// Cada um desses atributos deve se comportar como uma fila. 
+// Crie um método chamado estacionar() que deve receber uma string representando a 
+// placa de um carro (sem necessidade de validação) e armazenar o carro em um dos três andares. 
+// Cada andar tem o limite de 15 carros, se o primeiroAndar estiver cheio, 
+// deve armazenar no segundoAndar, e se este estiver cheio, o terceiroAndar começa a armazenar 
+// esses carros, o método deve imprimir no console em que andar cada carro foi adicionado. 
+// Se todos os três andares estiverem lotados, exiba uma mensagem de erro.
+
+// Crie também um método liberarVaga() que desaloca um carro do estacionamento.
